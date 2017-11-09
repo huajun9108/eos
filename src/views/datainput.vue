74f745cd2220fe2f9f28caa44e346f00655c3c78
Sponge <804927486@qq.com>
2017-11-09 09:42:29 +0800

    修改datainput界面样式中

--- src/views/datainput.vue
+++ src/views/datainput.vue
@@ -14,19 +14,28 @@
       <tbody>
         <tr v-for="d in tableData">
           <td class="firstCol">{{ d.tier }}</td>
-          <td class="secordCol">
+          <td class="secondCol">
             <textarea class="textArea"></textarea>
           </td>
           <td class="thirdCol">
             <div class="childTableContainer">
               <table class="childTableBody">
-              	<tr>
-              		<td>Tier3</td>
-                  <td>Tier4</td>
-                  <td>开始时间</td>
-                  <td>结束时间</td>
-                  <td>操作</td>
-              	</tr>
+                <tbody>
+                  <tr>
+                    <td class="childFirstCol">Tier3</td>
+                    <td class="childSecondCol">Tier4</td>
+                    <td class="childThirdCol">开始时间</td>
+                    <td class="childFourthCol">结束时间</td>
+                    <td class="childFifthCol">操作</td>
+                  </tr>
+                  <tr v-for="c in childTableData">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
               </table>
             </div>
           </td>
@@ -38,6 +47,9 @@
       </tbody>
     </table>
   </div>
+  <div class="LossChoose" :class="showFlag?'showchoose':'hidechoose'">
+    addLoss
+  </div>
 </div>
 </template>
 <script>
@@ -46,74 +58,99 @@ import echarts from "echarts"
 export default {
   data: function() {
     return {
-      tableData: [
-        { "tier": "OEE" },
-        { "tier": "OLE" },
-        { "tier": "MaintCosts" },
-        { "tier": "OEE" },
-        { "tier": "OLE" },
-        { "tier": "MaintCosts" },
-        { "tier": "OEE" },
-        { "tier": "OLE" },
-        { "tier": "MaintCosts" },
+      childTableData: [
+        {},
+        {},
+        {},
+        {},
+      ],
+      tableData: [{
+          "tier": "OEE"
+        },
+        {
+          "tier": "OLE"
+        },
+        {
+          "tier": "MaintCosts"
+        },
+        {
+          "tier": "OEE"
+        },
+        {
+          "tier": "OLE"
+        },
+        {
+          "tier": "MaintCosts"
+        },
+        {
+          "tier": "OEE"
+        },
+        {
+          "tier": "OLE"
+        },
+        {
+          "tier": "MaintCosts"
+        },
       ],
       value: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)],
       showFlag: false,
-      setting: {
-        async: {
-          enable: true,
-          url: "http://116.196.113.167:3001/areaAllSet/showAreaAll",
-          type: "get"
-        },
-        view: {
-          selectedMulti: false,
-          showIcon: false,
-        },
-        data: {
-          simpleData: {
-            enable: true
-          }
-        },
-        check: {
-          enable: true,
-          chkStyle: "checkbox",
-          radioType: "level"
-        },
-      },
+      // setting: {
+      //   async: {
+      //     enable: true,
+      //     url: "http://116.196.113.167:3001/areaAllSet/showAreaAll",
+      //     type: "get"
+      //   },
+      //   view: {
+      //     selectedMulti: false,
+      //     showIcon: false,
+      //   },
+      //   data: {
+      //     simpleData: {
+      //       enable: true
+      //     }
+      //   },
+      //   check: {
+      //     enable: true,
+      //     chkStyle: "checkbox",
+      //     radioType: "level"
+      //   },
+      // },
     }
   },
   methods: {
-    addLoss: function(){
+    addLoss: function() {
       const h = this.$createElement;
-        this.$msgbox({
-          title: '消息',
-          message: h('p', null, [
-            h('span', null, '内容可以是 '),
-            h('i', { style: 'color: teal' }, 'VNode')
-          ]),
-          showCancelButton: true,
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          beforeClose: (action, instance, done) => {
-            if (action === 'confirm') {
-              instance.confirmButtonLoading = true;
-              instance.confirmButtonText = '执行中...';
-              setTimeout(() => {
-                done();
-                setTimeout(() => {
-                  instance.confirmButtonLoading = false;
-                }, 300);
-              }, 3000);
-            } else {
+      this.$msgbox({
+        title: '消息',
+        message: h('p', null, [
+          h('span', null, '内容可以是 '),
+          h('i', {
+            style: 'color: teal'
+          }, 'VNode')
+        ]),
+        showCancelButton: true,
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        beforeClose: (action, instance, done) => {
+          if (action === 'confirm') {
+            instance.confirmButtonLoading = true;
+            instance.confirmButtonText = '执行中...';
+            setTimeout(() => {
               done();
-            }
+              setTimeout(() => {
+                instance.confirmButtonLoading = false;
+              }, 300);
+            }, 3000);
+          } else {
+            done();
           }
-        }).then(action => {
-          this.$message({
-            type: 'info',
-            message: 'action: ' + action
-          });
+        }
+      }).then(action => {
+        this.$message({
+          type: 'info',
+          message: 'action: ' + action
         });
+      });
     },
     getVirtulData: function(year) {
       year = year || '2017';

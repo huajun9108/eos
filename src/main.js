8f39d3a9674ac1e51dac4fe0cbe875ff535b5858
nicole <18766265@qq.com>
2017-11-15 13:37:46 +0800

    删除一些插件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import VueRouter from 'vue-router';
@@ -22,8 +20,6 @@ import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 
 Vue.use(VueRouter);
-Vue.use(iView);
-Vue.use(ElementUI, { size: 'small' })
 Vue.use(iView);
 Vue.config.productionTip = false
 
